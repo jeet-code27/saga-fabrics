c42bf032dba9743e235cb79146da1a35a944438d
Your Name <jetendrasinghratore1@gmail.com>
2026-09-25 17:00:55 +0530

    fix(analytics): ensure Meta Pixel base code is rendered directly in html head for instant crawler detection

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -107,6 +107,32 @@ export default function RootLayout({
     <html lang="en" className={`${cormorant.variable} ${inter.variable} h-full antialiased`}>
       <head>
         <JsonLdSchema />
+        {/* Meta Pixel Code */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '1808895900248579');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=1808895900248579&ev=PageView&noscript=1"
+            alt="Meta Pixel"
+          />
+        </noscript>
       </head>
       <body className="min-h-full flex flex-col bg-[#FAF6F1] text-[#2B2723] font-sans selection:bg-[#9E6962] selection:text-white">
         <GoogleAnalytics />
